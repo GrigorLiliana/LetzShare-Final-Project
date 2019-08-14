d1d6ed3d13859069aeadc8550c7b61326ed51555
Michel <michel.ds@live.fr>
2019-08-14 11:55:06 +0200

    updates

--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -8,6 +8,7 @@
     <title>{{ config('app.name', 'LetzShare') }}</title>
 
     <!-- Scripts -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/letzshare.js') }}" defer></script>
 
