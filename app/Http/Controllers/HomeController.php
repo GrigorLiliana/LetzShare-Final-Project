b1b0e067bedf19c93e2123ed32bc63164825310e
batecsw <batecsw@hotmail.com>
2019-08-14 11:57:31 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Photo;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,20 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $top3Pics = Photo::all()
+        ->orderBy('likesSum' , 'desc')
+        ->take(3)
+        ->get();
+        $recent3Pics = Photo::all()
+        ->orderBy('date' , 'desc')
+        ->take(3)
+        ->get();
+        $top3Users = User::all()
+        ->join('photos', 'users.user_id', '=', 'photos.user_id')
+        ->groupBy('photos.id_user')
+        ->orderBy( 'count(photos.id_photo)' , 'desc')
+        ->take(3)
+        ->get();
         return view('home');
     }
 }
