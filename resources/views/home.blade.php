ba6e9e0de7095fb368c594c108a5b756d2f92ace
Michel <michel.ds@live.fr>
2019-08-14 14:04:16 +0200

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+
 <p>This is the home.blade.php</p>
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    @if (Auth::check())
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-            </div>
+    <div class="row">
+        <div class="topPics">
+            <p>content of topPics</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="recentPics">
+            <p>content of recentPics</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="topUsers">
+            <p>content of topUsers</p>
         </div>
     </div>
 </div>
+
 @endsection
