583d69d88b796cf9700cda46812e9506956c8c3d
michel <michel.ds@live.fr>
2019-08-16 21:46:51 +0200

    homepage layout updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,63 +3,90 @@
 @section('content')
 
 <div class="container" id="homePage">
-    <div class="intro">
-        <h1>LetzShare - The beauty of Luxembourg</h1>
-        <p>LetzShare is a website for users to upload and share photographs of Luxembourg. These can be rated and commented upon by other users and the site will keep track of the most liked photos. Luxembourg is a small country nestling between France, Germany and Belgium and is not so well known outside the region.</p>
-        <p>By creating a platform to showcase attractive images of the Grand Duchy we hope to play a role in elevating its profile. Rich in history with many monuments and historic buildings this small country is also favoured by many attractive natural landscapes and beauty spots.</p>
-        <p>As well as this Luxembourg has its own unique culture and hugely diverse and multicultural population with a vibrant culture and nightlife reflecting this.
+
+    <div class="jumbotron">
+        <h1 class="display-4">LetzShare</h1>
+        <p class="lead">The beauty of Luxembourg</p>
+        <hr class="my-4">
+        <p class="blurb">LetzShare is a website for users to upload and share photographs of Luxembourg. These can be rated and commented upon by other users and the site will keep track of the most liked photos. Luxembourg is a small country nestling between France, Germany and Belgium and is not so well known outside the region.</p>
+        <p class="blurb">By creating a platform to showcase attractive images of the Grand Duchy we hope to play a role in elevating its profile. Rich in history with many monuments and historic buildings this small country is also favoured by many attractive natural landscapes and beauty spots.</p>
+        <p class="blurb">As well as this Luxembourg has its own unique culture and hugely diverse and multicultural population with a vibrant culture and nightlife reflecting this.
             Letzshare will be the platform which allows users to share images of all these aspects of Luxembourg.</p>
     </div>
-    <div class="topPics">
+
+    <div>
         <h3>Our Top Rated Photos</h3>
         <div class="row">
-            <?php
-            foreach ($topPics as $topPic) {
-                echo "<div class='col-sm'>";
-                $path = URL::asset($topPic->image_URL);
-                echo "<h5>$topPic->image_title</h5>
-                    <img class='homeGallery' src='$path' />
-                    <p>$topPic->image_description</p>
-                    <p>$topPic->likes_sum likes</p>
-                    </div>";
-                if (Auth::check())
-                    echo 'login';
-            }
-            ?>
+            <div class="topPics">
+                @foreach ($topPics as $topPic)
+                <div class="card" style="width: 30%;">
+                    <img src="{{ URL::asset($topPic->image_URL) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $topPic->image_title }}</h5>
+                        <p class="card-text">{{ $topPic->image_description }}</p>
+                        <div>
+                            <i class="fas fa-thumbs-up"></i>
+                            {{ $topPic->likes_sum }}
+                        </div>
+                        @if (Auth::check())
+                            <a href="#" class="btn btn-primary">Edit</a>   
+                        @endif
+                    </div>
+                  </div>
+                @endforeach
+            </div>
         </div>
     </div>
-    <div class="topUsers">
+    
+    <div>
         <h3>Our Top Photographers</h3>
         <div class="row">
-            <?php
-            foreach ($topUsers as $topUser) {
-                echo "<div class='col-sm'>";
-                $userId = $topUser->user_id;
-                $user = App\User::where('user_id', $userId)->first();
-    
-                $path = URL::asset($user->user_photo);
-                echo "<h5>$user->name</h5>
-                    <img class='homeGallery' src='$path' />
-                    <p> $user->name has posted $topUser->total_photos photos</p>
-                    </div>";
-            }
-            ?>
+            <div class="topUsers">
+                @foreach ($topUsers as $topUser)
+                @php
+                    $userId = $topUser->user_id;
+                    $user = App\User::where('user_id', $userId)->first();
+                @endphp
+                <div class="card" style="width: 30%;">
+                    <img src="{{ URL::asset($user->user_photo) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $user->name }}</h5>
+                        <p class="card-text">{{-- {{ $topUser->image_description }} --}}</p>
+                        <div>
+                            <i class="fas fa-images"></i>
+                            <span>has posted {{ $topUser->total_photos }} photos</span>
+                        </div>
+                        @if (Auth::check())
+                            <a href="#" class="btn btn-primary">Edit</a>   
+                        @endif
+                    </div>
+                  </div>
+                @endforeach
+            </div>
         </div>
     </div>
-    <div class="recentPics">
+    
+    <div>
         <h3>Our Latest Photos</h3>
         <div class="row">
-            <?php
-            foreach ($recentPics as $recentPic) {
-                echo "<div class='col-sm'>";
-                $path = URL::asset($recentPic->image_URL);
-                echo "<h5>$recentPic->image_title</h5>
-                    <img class='homeGallery' src='$path' />
-                    <p>$recentPic->image_description</p>
-                    <p>$recentPic->likes_sum likes</p>
-                    </div>";
-            }
-            ?>
+            <div class="recentPics">
+                @foreach ($recentPics as $recentPic)
+                <div class="card" style="width: 30%;">
+                    <img src="{{ URL::asset($recentPic->image_URL) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $topPic->image_title }}</h5>
+                        <p class="card-text">{{ $topPic->image_description }}</p>
+                        <div>
+                            <i class="fas fa-thumbs-up"></i>
+                            {{ $topPic->likes_sum }}
+                        </div>
+                        @if (Auth::check())
+                            <a href="#" class="btn btn-primary">Edit</a>   
+                        @endif
+                    </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div> <!-- /container-->
