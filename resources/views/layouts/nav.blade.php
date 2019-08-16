6bf7c7e6812c44e8a2a883f9ede66f966f832aa5
Michel <michel.ds@live.fr>
2019-08-16 15:30:16 +0200

    logo integrated in the navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,6 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('images/') }}/LetzShare_logo.png" alt="LetzShare logo." width="60px">
                     {{ config('app.name', 'LetzShare') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
