7895969de3219ae756a1cdc3e4f9ec8f4d422332
Liliana <liliana_irmg@hotmail.com>
2019-08-16 13:18:06 +0200

    on road

--- resources/views/uploadphoto.blade.php
+++ resources/views/uploadphoto.blade.php
@@ -9,8 +9,12 @@
         <div class="col-md-6 offset-3">
             <h3>Upload photo</h2>
             <div class="form-group">
-                <input type="file" name="image" class="form-control">
+            <label for="image">Select an amazing image </label>
+            <div class="custom-file">
+            <input type="file" class="custom-file-input" id="customFile">
+            <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
+        </div>
             <div class="form-group">
                 <label for="title">Title</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Enter the photo title" required>
@@ -19,19 +23,19 @@
                 <label for="description">Description</label>
                 <textarea class="form-control" id="description" name="description" rows="3" placeholder="This is an amazin photo taked by {{ Auth::user()->name }}" required></textarea>
             </div>
+
             <div class="form-group">
-                <label for="subject">Subject</label>
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Password" required>
+                <label for="locality">Locality</label>
+                <select name="locatily" id="locatily" required>
+                    <option value="">Belval</option>
+                </select>
+                <label for="category">Category</label>
+                <select name="category" id="category" required>
+                    <option value="">Nature</option>
+                </select>
             </div>
-            <div class="form-group">
-                <label for="message">Your message</label>
-                <textarea class="form-control" id="message" name="message" rows="3"></textarea>
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="sendEmail" name="sendEmail">
-                <label class="form-check-label" for="sendEmail">send me a copy per e-mail</label>
-            </div><br>
-            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+
+            <button type="submit" class="btn btn-primary btn-block">Upload</button>
         </div>
     </form>
 </div>
