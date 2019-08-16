f1846cdc97dd8ea4f04f13ceec89af429df3f139
batecsw <batecsw@hotmail.com>
2019-08-16 16:34:16 +0200

    Update to Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
 <h1>LetzShare - The beauty of Luxembourg</h1>
 <p id="blurb">LetzShare is a website for users to upload and share photographs of Luxembourg. These can be rated and commented upon by other users and the site will keep track of the most liked photos. Luxembourg is a small country nestling between France, Germany and Belgium and is not so well known outside the region.</p>
 <p id="blurb">By creating a platform to showcase attractive images of the Grand Duchy we hope to play a role in elevating its profile. Rich in history with many monuments and historic buildings this small country is also favoured by many attractive natural landscapes and beauty spots.</p>
 <p id="blurb">As well as this Luxembourg has its own unique culture and hugely diverse and multicultural population with a vibrant culture and nightlife reflecting this.
 Letzshare will be the platform which allows users to share images of all these aspects of Luxembourg.</p>
 <br>
+</div>
 <div class="container">
     <h3>Our Top Rated Photos</h3>
     <div class="row">
