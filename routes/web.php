6e3d4604538fc90cfe0eff951ecb20f4bced070c
Liliana <liliana_irmg@hotmail.com>
2019-08-16 09:36:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
