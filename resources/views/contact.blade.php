bde7d99bca40599cea42e1b7064ec45923223586
Michel <michel.ds@live.fr>
2019-08-16 10:05:15 +0200

    update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+    {{-- <div class="google-maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1295.5246824390836!2d5.948985344473696!3d49.50246177631499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479535034c0a600b%3A0xe8b8bed0e26f33a!2sTechnoport+S.a.!5e0!3m2!1sfr!2slu!4v1565942515575!5m2!1sfr!2slu" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div> --}}
     <form class="formbox">
         <div class="col-md-6 offset-3">
             <div class="form-group">
