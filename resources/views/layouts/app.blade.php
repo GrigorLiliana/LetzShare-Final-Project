b26df723493de55ae58b1194ed4f210ab66297e4
Ricardo <codzign@gmail.com>
2019-08-16 17:34:06 +0200

    gallery bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,10 @@
     <title>{{ config('app.name', 'LetzShare') }}</title>
 
     <!-- Scripts -->
+
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/letzshare.js') }}" defer></script>
-    <script src="{{ asset('js/jquery.justifiedGallery.js') }}"></script>
     <script src="https://kit.fontawesome.com/ff9603d652.js"></script>
 
     <!-- Fonts -->
@@ -40,14 +40,24 @@
 
     @include('layouts.footer')
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/ff9603d652.js"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/letzshare.js') }}" defer></script>
+    <script src="{{ asset('js/justifiedGallery.js') }}"></script>
     <script>
-        $(".gallery").justifiedGallery();
+        $('.gallery').justifiedGallery({
+            lastRow: 'nojustify',
+            rowHeight: 100,
+            margins: 1
+        }).on('jg.complete', function() {
+            $(this).find('a').colorbox({
+                maxWidth: '80%',
+                maxHeight: '80%',
+                opacity: 0.8,
+                transition: 'elastic',
+                current: ''
+            });
+        });
     </script>
 </body>
 
