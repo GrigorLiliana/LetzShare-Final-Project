0454b4e9039af34ce2b74bcba72dd675b340a75c
Ricardo <codzign@gmail.com>
2019-08-18 14:06:15 +0200

    gallery cards

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,15 +4,138 @@
 
 @section('content')
 
-<div class="gallery">
-    @foreach ($photos as $photo)
-    <a href="{{ $photo->image_URL }}">
-        <img src="{{ $photo->image_URL }}" class="gallery__img" alt="{{ $photo->image_title }}">
-    </a>
+<div class="row">
+@foreach ($photos as $photo)
+    <div class="col-sm-4">
+
+        <div class="card mb-3">
+            <a href="{{ $photo->image_URL }}">
+                <img src="{{ $photo->image_URL }}" class="card-img-top" alt="{{ $photo->image_title }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $photo->image_title }}</h5>
+                    <p class="card-text">{{ $photo->image_description }}</p>
+                    <p class="card-text">Author: <em>&mdash; {{ $photo->name }}</em></p>
+                    <ul>
+                        <li>
+                            <i class="fas fa-heart"></i>
+                            <span>{{ $photo->likes_sum }}</span></li>
+                        <li>
+                            <i class="fas fa-map-marker-alt"></i>
+                            <span>{{ $photo->locality_id }}</span></li>
+                        <li>
+                            @if($photo->category_id === 1)
+                            <i class="fas fa-landmark"></i>
+                            <span>Culture</span>
+                            @endif
+
+                            @if($photo->category_id === 2)
+                            <i class="fas fa-users"></i>
+                            <span>Events</span>
+                            @endif
+
+                            @if($photo->category_id === 4)
+                            <i class="fas fa-monument"></i>
+                            <span>Monuments</span>
+                            @endif
+
+                            @if($photo->category_id === 5)
+                            <i class="fas fa-tree"></i>
+                            <span>Nature</span>
+                            @endif
+
+                            @if($photo->category_id === 6)
+                            <i class="fas fa-glass-cheers"></i>
+                            <span>Night Life</span>
+                            @endif
+                        <li>
+                    </ul>
+                    <p class="card-text"><small class="text-muted">{{ $photo->date }}</small></p>
+                </div>
+            </a>
+        </div>
+
+    </div>
     @endforeach
 </div>
 
+<!-- Card -->
+<div class="card promoting-card">
+
+  <!-- Card content -->
+  <div class="card-body d-flex flex-row">
+
+    <!-- Avatar -->
+    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle mr-3" height="50px" width="50px" alt="avatar">
+
+    <!-- Content -->
+    <div>
+
+      <!-- Title -->
+      <h4 class="card-title font-weight-bold mb-2">{{ $photo->title }}</h4>
+      <!-- Subtitle -->
+      <p class="card-text"><i class="far fa-clock pr-2"></i>$photo->date</p>
+
+    </div>
+
+  </div>
+
+  <!-- Card image -->
+  <div class="view overlay">
+    <img class="card-img-top rounded-0" src="{{ $photo->image_URL }}" alt="Card image cap">
+    <a href="#!">
+      <div class="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+  <!-- Card content -->
+  <div class="card-body">
+
+    <div class="collapse-content">
+
+      <!-- Text -->
+      <p class="card-text collapse" id="collapseContent">{{ $photo->description }}</p>
+      <!-- Button -->
+      <ul>
+        <li>
+            <i class="fas fa-heart"></i>
+            <span>{{ $photo->likes_sum }}</span></li>
+        <li>
+            <i class="fas fa-map-marker-alt"></i>
+            <span>{{ $photo->locality_id }}</span></li>
+        <li>
+            @if($photo->category_id === 1)
+            <i class="fas fa-landmark"></i>
+            <span>Culture</span>
+            @endif
+
+            @if($photo->category_id === 2)
+            <i class="fas fa-users"></i>
+            <span>Events</span>
+            @endif
+
+            @if($photo->category_id === 4)
+            <i class="fas fa-monument"></i>
+            <span>Monuments</span>
+            @endif
+
+            @if($photo->category_id === 5)
+            <i class="fas fa-tree"></i>
+            <span>Nature</span>
+            @endif
+
+            @if($photo->category_id === 6)
+            <i class="fas fa-glass-cheers"></i>
+            <span>Night Life</span>
+            @endif
+        <li>
+          </ul>
+
+    </div>
+
+  </div>
 
 </div>
+<!-- Card -->∏
+
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/letzshare.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/ff9603d652.js"></script>
+    
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -25,7 +27,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/letzshare.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="{{ asset('css/justifiedGallery.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -43,22 +44,6 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/justifiedGallery.js') }}"></script>
-    <script>
-        $('.gallery').justifiedGallery({
-            lastRow: 'nojustify',
-            rowHeight: 100,
-            margins: 1
-        }).on('jg.complete', function() {
-            $(this).find('a').colorbox({
-                maxWidth: '80%',
-                maxHeight: '80%',
-                opacity: 0.8,
-                transition: 'elastic',
-                current: ''
-            });
-        });
-    </script>
 </body>
 
 </html>
