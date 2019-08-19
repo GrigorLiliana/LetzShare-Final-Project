086f2a296851aae09927fdc038daa1a000d9d5dc
michel <michel.ds@live.fr>
2019-08-19 15:39:57 +0200

    jquery updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,9 @@
     @include('layouts.footer')
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/ff9603d652.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/letzshare.js') }}"></script>
 </body>
 
 </html>
