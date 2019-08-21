355eba41c131e69830c1ac9338085887ee11724e
michel <michel.ds@live.fr>
2019-08-21 12:43:07 +0200

    view: about us updated

--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -1,8 +1,7 @@
-<footer>
-<div class="container">
-    <p>This is the footer (footer.blade.php)</p>
-</div>
-</footer>
+<footer class="container m-t-2">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2019 LetzShare &middot; Final project &middot; <a href="#fit4coding">#Fit4CodingJobs Session 5</a> &middot; <a href="#privacy">Privacy</a> &middot; <a href="#terms">Terms & Conditions</a></p>
+  </footer>
 
 
 
