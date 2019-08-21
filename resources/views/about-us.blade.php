c242b5a1304d34a6200372f25df05d7b26a3e8f5
michel <michel.ds@live.fr>
2019-08-21 15:46:14 +0200

    about-us view update

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -4,8 +4,6 @@
 
 @section('content')
 
-
-
 <main role="main">
 
     <div class="jumbotron">
@@ -27,39 +25,59 @@
             <div class="row">
                 <div class="col-md-6 col-lg-6 mb-5">
                     {{-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> --}}
-                <div class="">
-                    <img src="{{ asset('uploads/avatars-admin/liliana.jpg')}}" alt="" class="rounded-circle mb-3 user-profile" width="150px" height="150px">
-                </div>
-                    <h2>Liliana</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
-                <p><a class="btn btn-secondary" href="{{ url('userprofile/10') }}" role="button">View profile &raquo;</a></p>
+                    <div class="">
+                        <a href="{{ url('userprofile/10') }}"><img src="{{ asset('uploads/avatars-admin/liliana.jpg')}}" alt="Liliana" title="Liliana" class="rounded-circle mb-3 img-thumbnail" width="150px" height="150px"></a>
+                    </div>
+                    <div class="m-3">   
+                        <h2>Liliana</h2>
+                        <p>Full Stack Web Developer</p>
+                        <p>Back-End: PHP, PHP OOP, Laravel, MySQL Database, SQL, Wordpress</p>
+                            
+                        <p>Front-End: Web Development & Integration: HTML5, CSS3, SASS, Bootstrap, JavaScript, jQuery, Angular, Ajax, Cordova, Ionic, UI/ UX design, SEO</p>
+                        <p><a class="btn btn-primary" href="{{ url('userprofile/10') }}" role="button">View profile &raquo;</a></p>
+                    </div>
                 </div>
                 
                 <div class="col-md-6 col-lg-6 mb-5">
                     <div class="">
-                        <img src="{{ asset('uploads/avatars-admin/stuart.jpg')}}" alt="" class="rounded-circle mb-3 user-profile" width="150px" height="150px">
+                        <a href="{{ url('userprofile/9') }}"><img src="{{ asset('uploads/avatars-admin/stuart.jpg')}}" alt="Stuart" title="Stuart" class="rounded-circle mb-3 img-thumbnail" width="150px" height="150px"></a>
                     </div>
-                    <h2>Stuart</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
-                    <p><a class="btn btn-secondary" href="{{ url('userprofile/9') }}" role="button">View profile &raquo;</a></p>
+                    <div class="m-3">   
+                        <h2>Stuart</h2>
+                        <p>Full Stack Web Developer</p>
+                        <p>Back-End: PHP, PHP OOP, Laravel, MySQL Database, SQL, Wordpress</p>
+                            
+                        <p>Front-End: Web Development & Integration: HTML5, CSS3, SASS, Bootstrap, JavaScript, jQuery, Angular, Ajax, Cordova, Ionic, UI/ UX design, SEO</p>
+                        <p><a class="btn btn-primary" href="{{ url('userprofile/9') }}" role="button">View profile &raquo;</a></p>
+                    </div>
                 </div>
 
-                <div class="col-md-6 col-lg-6 mb-5">
+                <div class="col-md-6 col-lg-6 m-auto">
                     <div class="">
-                        <img src="{{ asset('uploads/avatars-admin/ricardo.jpg')}}" alt="" class="rounded-circle mb-3 user-profile" width="150px" height="150px">
+                        <a href="{{ url('userprofile/21') }}"><img src="{{ asset('uploads/avatars-admin/ricardo.jpg')}}" alt="Ricardo" title="Ricardo" class="rounded-circle mb-3 img-thumbnail" width="150px" height="150px"></a>
                     </div>
-                    <h2>Ricardo</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
-                    <p><a class="btn btn-secondary" href="{{ url('userprofile/21') }}" role="button">View profile &raquo;</a></p>
+                    <div class="m-3">   
+                        <h2>Ricardo</h2>
+                        <p>Full Stack Web Developer</p>
+                        <p>Back-End: PHP, PHP OOP, Laravel, MySQL Database, SQL, Wordpress</p>
+                            
+                        <p>Front-End: Web Development & Integration: HTML5, CSS3, SASS, Bootstrap, JavaScript, jQuery, Angular, Ajax, Cordova, Ionic, UI/ UX design, SEO</p>
+                        <p><a class="btn btn-primary" href="{{ url('userprofile/21') }}" role="button">View profile &raquo;</a></p>
+                    </div>
                 </div>
 
-                <div class="col-md-6 col-lg-6 mb-5">
+                <div class="col-md-6 col-lg-6 m-auto">
                     <div class="">
-                        <img src="{{ asset('uploads/avatars-admin/michel.jpg')}}" alt="" class="rounded-circle mb-3 user-profile" width="150px" height="150px">
+                        <a href="{{ url('userprofile/1') }}"><img src="{{ asset('uploads/avatars-admin/michel.jpg')}}" alt="Michel" title="Michel" class="rounded-circle mb-3 img-thumbnail" width="150px" height="150px"></a>
                     </div>
-                    <h2>Michel</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
-                    <p><a class="btn btn-secondary" href="{{ url('userprofile/1') }}" role="button">View profile &raquo;</a></p>
+                    <div class="m-3">   
+                        <h2>Michel</h2>
+                        <p>Full Stack Web Developer</p>
+                        <p>Back-End: PHP, PHP OOP, Laravel, MySQL Database, SQL, Wordpress</p>
+                            
+                        <p>Front-End: Web Development & Integration: HTML5, CSS3, SASS, Bootstrap, JavaScript, jQuery, Angular, Ajax, Cordova, Ionic, UI/ UX design, SEO</p>
+                        <p><a class="btn btn-primary" href="{{ url('userprofile/1') }}" role="button">View profile &raquo;</a></p>
+                    </div>
                 </div>
             </div><!-- /.row -->
         </div><!-- /.card-body -->
