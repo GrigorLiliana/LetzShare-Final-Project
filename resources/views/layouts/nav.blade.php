69077ba0e6054aaa186c6569f7b0bd77a7dcc87f
Liliana <liliana_irmg@hotmail.com>
2019-08-21 12:41:56 +0200

    edit name

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -51,7 +51,7 @@
                 @else
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle text-capitalize" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
 
