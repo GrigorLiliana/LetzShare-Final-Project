dcd993e1c3a58e38f7ac837bdc69173c418688a4
Liliana <liliana_irmg@hotmail.com>
2019-08-23 14:53:38 +0200

    REGISTER

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -94,13 +94,4 @@
 
     </nav>
 </header>
-<div class="notAdmin">
-    @if ( $message = Session::get('error') )
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        <span>{{ $message }}</span>
-    </div>
-    @endif
-</div>
+
