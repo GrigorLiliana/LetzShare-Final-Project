57be951ce27a12681c55740b310a12e11284a48e
Ricardo Ribeiro <kodii@kodii.eu>
2019-08-27 09:51:54 +0200

    commit

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -16,7 +16,7 @@
             </h5>
         </div>
 
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
             <form method="POST" class="form-filters">
                 @csrf
                 <div class="form-row">
